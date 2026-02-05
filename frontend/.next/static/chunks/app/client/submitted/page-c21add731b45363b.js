8c9e037b74ace94b794c015382e3ab8d184f70f9
GodHercules <hercules.oliveira@ba.estudante.senai.br>
2026-02-05 17:19:57 -0300

    feat subindo

---
+++ frontend/.next/static/chunks/app/client/submitted/page-c21add731b45363b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[494],{6389:function(n,e,u){Promise.resolve().then(u.t.bind(u,9353,23))}},function(n){n.O(0,[585,509,901,744],function(){return n(n.s=6389)}),_N_E=n.O()}]);
